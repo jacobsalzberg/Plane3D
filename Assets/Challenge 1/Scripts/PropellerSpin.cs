275e4d668997c6c824ed329830682b862286c937
Jacob Salzberg <jacob1818@gmail.com>
2021-02-19 19:30:23 -0300

    Fix offset and add rotation script

---
+++ Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float propellerSpeed=1200f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,propellerSpeed*Time.deltaTime,Space.Self);
+    }
+}
